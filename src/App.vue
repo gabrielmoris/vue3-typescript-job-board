c9347b9eab0e40b2efe9094741c51be9e3fa2ae1
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-06-25 21:47:40 +0200

    add jobs and create types folder

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,51 @@
 <template>
-<div class="app">
-  Hello world
-</div>
+    <div class="app"> Hello world </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent, ref } from "vue";
+import Job from "./types/Job";
 
 export default defineComponent({
-  name: 'App',
-  components: {
+    name: "App",
+    components: {},
+    setup() {
+        const jobs = ref<Job[]>([
+            {
+                title: "Farm worker",
+                location: "Cecebre",
+                salary: 30000,
+                id: "1",
+            },
+            {
+                title: "Musician",
+                location: "Culleredo",
+                salary: 50000,
+                id: "2",
+            },
+            {
+                title: "Web Developer",
+                location: "Berlin",
+                salary: 80000,
+                id: "3",
+            },
+            {
+                title: "Busines developement",
+                location: "Switzerland",
+                salary: 100000,
+                id: "4",
+            },
+            {
+                title: "Waiter",
+                location: "Coruña",
+                salary: 20000,
+                id: "5",
+            },
+        ]);
 
-  }
+        return { jobs };
+    },
 });
 </script>
 
-<style>
-
-</style>
+<style></style>
